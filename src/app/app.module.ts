aa5fe764581758da5cb819f81094fed20cbffd82
zigzi salah eddine <s.zigzi@gmail.com>
2019-06-01 03:37:08 +0000

    add routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
 import { PlayersListComponent } from './players-list/players-list.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {PlayerService} from './shared/services/player.service';
+import { PlayerAddComponent } from './player-add/player-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    PlayersListComponent
+    PlayersListComponent,
+    PlayerAddComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +48,14 @@ import {PlayerService} from './shared/services/player.service';
     MatPaginatorModule,
     MatSortModule,
     HttpClientModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    RoutingModule
   ],
   providers: [HttpClient, PlayerService],
   bootstrap: [AppComponent]
